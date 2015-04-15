04746bd332a100e9dc6654a4ef1cee065f3bec86
Tara Mahoney <tara.mahoney@me.com>
2015-04-15 16:41:15 -0400

    Added the ability to view individual pictures on their own pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-get "pictures" => "pictures#index"
-
+  get "pictures" => "pictures#index"
+  get "pictures/:id" => "pictures#show", as: "picture"
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +56,4 @@ get "pictures" => "pictures#index"
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
